052d78038677a24b3f81ec2e522a50e8a546734e
Claus Krag <maxkrag@gmail.com>
2016-02-19 19:34:18 +0100

    Implemented some initial error handling and logging

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from multiprocessing.dummy import Pool as ThreadPool
 import time
 import json
 import os.path
+import datetime
 from database import SqlConnector
+from socket import error as SocketError
 
 
 class Responser(object):
@@ -12,6 +14,7 @@ class Responser(object):
     db = None
     source_filename = "sources.json"
     template_path = "templates/"
+    logging_path = "logging/"
     source_template_filename = "sources_template.json"
     userAgent = 'Your friendly neighborhood API-response-tracker - https://github.com/ckrag/responder'
 
@@ -64,21 +67,43 @@ class Responser(object):
     def time_url_opening(self, source):
         url = source["url"]
         pretime = time.time()
-        request_result = requests.get(url=url, data=None, headers={'Connection':'close', 'user-agent':self.userAgent})
+        try:
+            request_result = requests.get(url=url, data=None, headers={'Connection':'close', 'user-agent':self.userAgent})
+        except ConnectionError as e:
+            self.log_error(e)
+            return None
+        except SocketError as e:
+            self.log_error(e)
+            return None
+
         response_code = request_result.status_code
         request_result.close()
         reques_time = time.time() - pretime
         return { "time" : reques_time, "url" : url, "code" : response_code}
+
+
+    def format_time(self, time_string):
+        if len(time_string) == 0:
+            return '0' + time_string
+        return time_string
 
     def get_response_times(self):
         pool = ThreadPool(4)
         current_time = int(time.time())
 
         results = pool.map(self.time_url_opening, self.sources_cache)
+
         pool.close()
         pool.join()
 
-        self.db.store_data(results, current_time)
+        # For now we simply ignore any collections with None values, since None values aren't supported in the database
+        if not None in results:
+            self.db.store_data(results, current_time)
+
+    def log_error(self, exception):
+        timestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+        with open(self.logging_path + "request_log", 'a') as log_file:
+            log_file.write("\n" + timestamp + "\n" + exception + "\n")
 
     def run(self):
         try:
