51e67797e8ad4df8c09accdd3a7584dfdafc5082
Claus Krag <maxkrag@gmail.com>
2016-02-19 21:49:55 +0100

    moved source setup to utils class

--- main.py
+++ main.py
@@ -6,63 +6,31 @@ import os.path
 import datetime
 from database import SqlConnector
 from socket import error as SocketError
+from utils import Util
 
 
 class Responser(object):
 
     request_frequency_sec = 3
     db = None
-    source_filename = "sources.json"
-    template_path = "templates/"
     logging_path = "logging/"
-    source_template_filename = "sources_template.json"
     userAgent = 'Your friendly neighborhood API-response-tracker - https://github.com/ckrag/responder'
 
     sources_cache = None
 
     def __init__(self):
-        if self.init():
+        if self.initialize():
             self.run()
 
-    def init(self):
-        if os.path.isfile(self.source_filename):
-            with open(self.source_filename) as source_data:
-                sources = json.load(source_data)["sources"]
-                if len(sources) > 0:
-                    invalid_sources = self.get_invalid_sources(sources)
-                    if len(invalid_sources) == 0:
-                        self.db = SqlConnector()
-                        self.sources_cache = sources
-                        print("Successfully loaded " + str(len(sources)) + " source(s)")
-                        return True
-                    else:
-                        print("Found sources, but these sources were invalid: ")
-                        for source_string in invalid_sources:
-                            print(source_string)
-                        print("Please open and correct " + self.source_filename + " in project root. Remember to add http / https")
-                else:
-                    print("Found empty source, please append URIs to the sources list inside " + self.source_filename + " in the project-root-directory")
-        else:
-            with open(self.template_path + self.source_template_filename, 'r') as template:
-                source_template = template.read()
-                with open(self.source_filename, 'w') as new_source_file:
-                    new_source_file.write(source_template)
-            print("Was unable to find " + self.source_filename + ", created empty source, please append URIs to the sources list inside " + self.source_filename + " in the project-root-directory")
+    def initialize(self):
+        sources = Util.get_source_content()
+        if sources is not None:
+            self.db = SqlConnector()
+            self.sources_cache = sources
+            return True
         return False
 
-    def get_invalid_sources(self, sources):
-        sUri = "https://"
-        uri = "http://"
-        invalid_uris = []
 
-        for source in sources:
-            source_url = source["url"]
-            if source_url[:len(sUri)] == sUri or source_url[:len(uri)] == uri:
-                pass
-            else:
-                invalid_uris.append(source_url)
-
-        return invalid_uris
 
     def time_url_opening(self, source):
         url = source["url"]
---
+++ utils.py
@@ -0,0 +1,49 @@
+import os.path
+import json
+
+class Util(object):
+
+    @staticmethod
+    def get_source_content():
+        template_path = "templates/"
+        source_filename = "sources.json"
+        source_template_filename = "sources_template.json"
+
+        if os.path.isfile(source_filename):
+            with open(source_filename) as source_data:
+                sources = json.load(source_data)["sources"]
+                if len(sources) > 0:
+                    invalid_sources = Util.__get_invalid_sources(sources)
+                    if len(invalid_sources) == 0:
+
+                        print("Successfully loaded " + str(len(sources)) + " source(s)")
+                        return sources
+                    else:
+                        print("Found sources, but these sources were invalid: ")
+                        for source_string in invalid_sources:
+                            print(source_string)
+                        print("Please open and correct " + source_filename + " in project root. Remember to add http / https")
+                else:
+                    print("Found empty source, please append URIs to the sources list inside " + source_filename + " in the project-root-directory")
+        else:
+            with open(template_path + source_template_filename, 'r') as template:
+                source_template = template.read()
+                with open(source_filename, 'w') as new_source_file:
+                    new_source_file.write(source_template)
+            print("Was unable to find " + source_filename + ", created empty source, please append URIs to the sources list inside " + source_filename + " in the project-root-directory")
+        return None
+
+    @staticmethod
+    def __get_invalid_sources(sources):
+        sUri = "https://"
+        uri = "http://"
+        invalid_uris = []
+
+        for source in sources:
+            source_url = source["url"]
+            if source_url[:len(sUri)] == sUri or source_url[:len(uri)] == uri:
+                pass
+            else:
+                invalid_uris.append(source_url)
+
+        return invalid_uris
