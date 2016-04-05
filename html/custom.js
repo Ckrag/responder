b88c4deb869f4ddc570b66dcc9aebe5879ea43eb
Claus Krag <maxkrag@gmail.com>
2016-04-05 14:42:08 +0200

    update chart.js to 2.0 beta, implemented responsecodes and reworked design

--- html/custom.js
+++ html/custom.js
@@ -1,5 +1,5 @@
-var requestInterval = 3000;
-var mainChart = null;
+var requestInterval = 5000;
+var lineChart = null;
 var timespan = 40;
 
 var chartData = null;
@@ -13,11 +13,6 @@ $( document ).ready(function() {
 function init(){
   // Show some loading or something til first ajax request returns
   startEventLoop();
-}
-
-function addApiVisual(name, color){
-  var html_string = "<div class='api_entry'><p class='api_name'><span style='background-color:" + color + ";'class='api_color'></span>" + name + "</p></div>";
-  $("#api_info_box").append(html_string);
 }
 
 function loadData(){
@@ -41,7 +36,7 @@ function loadData(){
 function onDataLoaded(jsonData){
   var dataObject = JSON.parse(jsonData);
 
-  if(dataObject.api_count == 0){
+  if(dataObject.api_count === 0){
     // No apis, do nothing.
     // When we handle changing number of apis, start out drawing an empty
     // Some day :o)
@@ -56,13 +51,69 @@ function onDataLoaded(jsonData){
 }
 
 function configChart(chart, chartData){
-  var chartContext = document.getElementById('main-chart').getContext('2d');
-  mainChart = new Chart(chartContext).Line(chartData, {
-    scaleOverride : true,
-    scaleSteps : 3,
-    scaleStepWidth: 1,
-    scaleStartValue: 0
+  var chart = document.getElementById('main-chart');
+  var chartContext = chart.getContext('2d');
+
+  Chart.defaults.global.tooltips.enabled = true;
+  Chart.defaults.global.tooltips.bodyFontStyle = "normal";
+  Chart.defaults.global.tooltips.footerFontStyle = "normal";
+
+  Chart.defaults.global.tooltips.titleFontSize = 15;
+  Chart.defaults.global.tooltips.bodyFontSize = 15;
+  Chart.defaults.global.tooltips.footerFontSize = 15;
+
+  Chart.defaults.global.tooltips.callbacks.title = function(tooltipItem, data) { 
+    return data.datasets[tooltipItem[0].datasetIndex].label;
+  }
+
+  Chart.defaults.global.tooltips.callbacks.label = function(tooltipItem, data) {
+    return tooltipItem.xLabel;
+  }
+
+  Chart.defaults.global.tooltips.callbacks.beforeFooter = function(tooltipItem, data) {
+    return tooltipItem[0].yLabel;
+  }
+
+
+  Chart.defaults.global.tooltips.callbacks.footer = function(tooltipItem, data) { 
+    return data.responseCodeSets[tooltipItem[0].datasetIndex][tooltipItem[0].index];
+  }
+
+
+  lineChart = new Chart(chartContext, {
+    type: 'line',
+    data: chartData,
+    options: {
+      scaleOverride : true,
+      scaleSteps : 3,
+      scaleStepWidth: 1,
+      scaleStartValue: 0,
+      onAnimationProgress: function(){
+      },
+      onAnimationComplete: function(){
+      }
+    }
   });
+}
+
+function getResponseCode(evt){
+  value = evt.value;
+  label = evt.label;
+
+  for (var i = 0; i < chartData.labels.length; i++) {
+    if(chartData.labels[i] === label){
+      labelMatchIndex = i;
+
+
+      for (var j = 0; j < chartData.datasets.length; j++) {
+        if(chartData.datasets[j][i] == value){
+          return chartData.responseCodeSets[j][i];
+        }
+      };
+    }
+  };
+
+  return -1;
 }
 
 function createChartData(dataObject){
@@ -70,15 +121,17 @@ function createChartData(dataObject){
   chartData = {
     labels: [],
     datasets: [],
-    unixTimeStamps: []
+    unixTimeStamps: [],
+    responseCodeSets: []
   };
   
   var api_count = dataObject.api_count;
-  var api_names = dataObject.api_names;
+  var api_meta_data = dataObject.api_meta_data;
   var data = dataObject.api_data;
 
   for (var i = 0; i < api_count; i++) {
-    chartData.datasets.push(createDataSet(api_names[i]));
+    chartData.datasets.push(createDataSet(api_meta_data[i]));
+    chartData.responseCodeSets.push([]);
   };
 
   for (var i = 0; i < data.length; i++) {
@@ -88,35 +141,32 @@ function createChartData(dataObject){
     var responses = data[i].api_responsetimes;
 
     for (var j = 0; j < responses.length; j++) {
-      chartData.datasets[j].data.push(responses[j].response_time); 
+      chartData.datasets[j].data.push(responses[j].response_time);
+      chartData.responseCodeSets[j].push(responses[j].response_code);
     };
   };
 
   // setup chart
-  configChart(mainChart, chartData);
+  configChart(lineChart, chartData);
 
 }
 
-function createDataSet(apiName){
-  var color = getRandomColors(1)[0];
-  colorApiMap.push({ 
-    name : apiName,
-    color : color
-   });
+function createDataSet(api_meta_data){
+  var name = (api_meta_data.pretty_name.length != 0) ? api_meta_data.pretty_name : api_meta_data.request_url;
+  var color = (api_meta_data.pretty_color.length != 0) ? api_meta_data.pretty_color : getRandomColors(1)[0];
 
-  addApiVisual(apiName, color);
+  rgbColor = hexToRgbaString(color, 1);
 
   return {
-      fillColor : "rgba(172,194,132,0.0)",
-      strokeColor : color,
-      pointColor : "#fff",
-      pointStrokeColor : "#9DB86D",
+      borderColor : hexToRgbaString(color, 0.5),
+      pointBorderColor : hexToRgbaString(color, 1),
+      fill: false,
+      backgroundColor : hexToRgbaString(color, 1),
       data : [],
-      name : apiName
+      label : name
     }
 }
 
-// WHAT IF WE DONT HAVE MAX YET. NEW DATA CAN HOLD LONGER SETS !!!!!
 function updateChartData(dataObject){
 
   var api_count = dataObject.api_count;
@@ -125,7 +175,8 @@ function updateChartData(dataObject){
 
   var newLabels = [];
   var newResponseTimeSets = [];
-  var newReponseTimestamps = []
+  var newResponseCodeSets = [];
+  var newReponseTimestamps = [];
 
   for (var i = 0; i < data.length; i++) {
     newLabels[i] = convertUnixToReadable(data[i].timestamp);
@@ -134,11 +185,14 @@ function updateChartData(dataObject){
     var responses = data[i].api_responsetimes;
 
     var responseSet = [];
+    var responseCodeSet = [];
 
     for (var j = 0; j < responses.length; j++) {
       responseSet.push(responses[j].response_time);
+      responseCodeSet.push(responses[j].response_code);
     };
     newResponseTimeSets.push(responseSet);
+    newResponseCodeSets.push(responseCodeSet);
   };
 
   var offsetIndex = getArrayChangeOffset(newReponseTimestamps, chartData.unixTimeStamps);
@@ -146,8 +200,8 @@ function updateChartData(dataObject){
   for (var i = 0; i < offsetIndex; i++) {
 
     // Don't remove old entries until the graph shows full 'timespan'
-    if(chartData.labels.length == timespan){
-      mainChart.removeData();
+    if(chartData.labels.length === timespan){
+      lineChart.removeData();
     }
 
     var newData = [];
@@ -156,15 +210,19 @@ function updateChartData(dataObject){
       //console.log("response lookup: " + newResponseTimeSets.length + " - 1 - " + i + "["+j+"]");
       newData.push(newResponseTimeSets[newResponseTimeSets.length - offsetIndex + i][j]);
     };
-    mainChart.addData(newData, newLabels[newLabels.length - offsetIndex + i]);
-  };
+    // Push new data to cause graphical update
+    lineChart.addData(newData, newLabels[newLabels.length - offsetIndex + i]);
 
+  };
+  
+  // Update the data
   chartData.unixTimeStamps = newReponseTimestamps;
   for (var i = 0; i < newResponseTimeSets.length; i++) {
     for (var j = 0; j < chartData.datasets.length; j++) {
       chartData.datasets[j][i] = newResponseTimeSets[i][j];
+      chartData.responseCodeSets[j][i] = newResponseCodeSets[i][j];
     };
-  };
+  };  
 }
 
 // Method expects two arrays of equal length
@@ -184,7 +242,7 @@ function getArrayChangeOffset(newArray, oldArray){
     var matchCount = 0;
 
     for (var j = 0; j < oldArray.length - i; j++) {
-      if(oldArray[j+i] == newArray[j]){
+      if(oldArray[j+i] === newArray[j]){
         matchCount++;
       }
     }
@@ -231,15 +289,15 @@ function convertUnixToReadable(unixTimeStamp){
   var minutes = String(date.getMinutes());
   var seconds = String(date.getSeconds());
 
-  if(hours.length == 1){
+  if(hours.length === 1){
     hours = "0" + hours;
   }
 
-  if(minutes.length == 1){
+  if(minutes.length === 1){
     minutes = "0" + minutes;
   }
 
-  if(seconds.length == 1){
+  if(seconds.length === 1){
     seconds = "0" + seconds;
   }
 
@@ -254,4 +312,25 @@ function startEventLoop(){
       loadData();
   };
   setInterval(action, requestInterval);
+}
+
+function hexToRgb(hex) {
+  // FULL CREDIT GOES TO USER 'TIM DAWN' - http://stackoverflow.com/a/5624139
+    // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+        return r + r + g + g + b + b;
+    });
+
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+}
+
+function hexToRgbaString(hex, alpha){
+  rgbObj = hexToRgb(hex);
+  return "rgba(" + rgbObj.r + "," + rgbObj.g + "," + rgbObj.b + "," + alpha + ")";
 }
